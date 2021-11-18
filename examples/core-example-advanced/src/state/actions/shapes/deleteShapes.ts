a7cf34e3b3ff733513f710607b079bf00fa8734f
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2021-11-18 14:03:08 -0800

    fix: deletion should work

--- examples/core-example-advanced/src/state/actions/shapes/deleteShapes.ts
+++ examples/core-example-advanced/src/state/actions/shapes/deleteShapes.ts
@@ -2,10 +2,6 @@ import type { Action } from 'state/constants'
 
 export const deleteShapes: Action = (data, payload: { ids: string[] }) => {
   try {
-    data.pageState.selectedIds = data.pageState.selectedIds.filter(
-      (id) => !payload.ids.includes(id)
-    )
-
     payload.ids.forEach((id) => {
       delete data.page.shapes[id]
     })
